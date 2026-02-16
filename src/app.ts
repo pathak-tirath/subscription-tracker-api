6b8462112eb30dcc5d3276f8e8087e49e132761c
Tirath Pathak <pathaktirath2000@gmail.com>
2026-02-16 09:26:00 +0530

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import swaggerUi from 'swagger-ui-express'
+import swaggerUi from "swagger-ui-express";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
-
+import cors from "cors";
 import { PORT } from "@/config/env";
 import authRouter from "@/routes/auth.routes";
 import userRouter from "./routes/user.routes";
@@ -11,22 +11,24 @@ import logger from "@/utils/logger";
 import { connectToDB } from "@/database/db";
 import { errorMessage } from "@/middlewares/error.middleware";
 import { arcjetMiddleware } from "@/middlewares/arcjet.middleware";
-import swaggerFile from './swagger/swagger-output.json'
+import swaggerFile from "./swagger/swagger-output.json";
 
 const app = express();
+
+app.use(cors());
 
 // * Helmet security middleware
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(arcjetMiddleware)
+app.use(arcjetMiddleware);
 
 // Routers
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users",userRouter);
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
-app.use("/api-doc", swaggerUi.serve,swaggerUi.setup(swaggerFile));
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // middlewares
 app.use(errorMessage);
